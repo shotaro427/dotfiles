d0642bda0bc5d41a96f6016cb35d5952ed7ef7e6
shotaro427 <shotaro.tu13@gmail.com>
2025-04-10 15:19:40 +0900

    sleep apt update

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# リトライ付き apt update
+# 最大6 * 10秒 = 60秒待つ
+for i in {1..6}; do
+  if sudo apt update; then
+    break
+  else
+    echo "apt update failed, retrying in 10 seconds..."
+    sleep 10
+  fi
+done
+
 # 必要なパッケージのインストールチェックと実行
 check_and_install() {
     if ! command -v $1 &> /dev/null; then
