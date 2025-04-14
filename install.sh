e7a94504fb3addbc2ffc464dafaa7b1cbd1b78cc
shotaro427 <shotaro.tu13@gmail.com>
2025-04-14 11:01:22 +0900

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-# リトライ付き apt update
-# 最大6 * 10秒 = 60秒待つ
-for i in {1..6}; do
-  if sudo apt update; then
-    break
-  else
-    echo "apt update failed, retrying in 10 seconds..."
-    sleep 10
-  fi
-done
 
 # 必要なパッケージのインストールチェックと実行
 check_and_install() {
     if ! command -v $1 &> /dev/null; then
-        echo "$1 をインストールします..."
+        echo "✨️✨️✨️✨️ $1 をインストールします..."
         sudo apt-get install -y $1
     else
-        echo "$1 は既にインストールされています"
+        echo "✨️✨️✨️✨️ $1 は既にインストールされています"
     fi
 }
 
+# リトライ付き apt update
+# 最大6 * 10秒 = 60秒待つ
+echo "✨️✨️✨️✨️ システムをアップデートします..."
+for i in {1..6}; do
+  echo "✨️✨️✨️✨️ aptの更新を試みています... $(($i))回目"
+  if sudo apt update; then
+    echo "✨️✨️✨️✨️ aptの更新が完了しました"
+    break
+  else
+    echo "✨️✨️✨️✨️ aptの更新に失敗しました。10秒後に再試行します..."
+    sleep 10
+  fi
+done
+
 # システムのアップデート
-echo "システムをアップデートします..."
-sudo apt-get update
+echo "✨️✨️✨️✨️ システムをアップグレードします..."
 sudo apt-get upgrade -y
 
 # 必要なパッケージのインストール
